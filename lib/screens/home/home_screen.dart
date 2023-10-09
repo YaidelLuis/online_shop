acfb1ff8ca87290c0fa2b0335889afc932577c2b
YaidelLuis <yaidelaraujo@gmail.com>
2023-10-09 18:22:08 +0200

    commit test

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -48,6 +48,7 @@ class HomeScreen extends StatelessWidget {
                   .copyWith(fontWeight: FontWeight.bold),
             ),
           ),
+          //yaidel
           Categories(),
           Expanded(
             child: Padding(
